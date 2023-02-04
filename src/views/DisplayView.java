cb2b16e008949d02ae53a6f628ddcf0fe5136f4a
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-02-05 04:51:39 +0700

    Menambahkan display

---
+++ src/views/DisplayView.java
@@ -0,0 +1,16 @@
+package views;
+
+// Library
+import javax.swing.JLabel;
+
+// Class
+public class DisplayView extends JLabel {
+    
+    // Constructor
+    public DisplayView () {
+
+        
+
+    }
+
+}
